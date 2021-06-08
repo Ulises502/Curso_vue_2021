4efc10b78763f3d2b4a65fbc2b9229f0f52a3016
Ulises502 <43120036+Ulises502@users.noreply.github.com>
2021-06-08 16:37:27 -0300

    Crear un layout base para la app
    Agrega un Navbar, sidebar y footer q usan atributo "app"
    permitiendoles juntarse y no superponerse

---
+++ trabajo_final/src/components/layout/Navigation.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-navigation-drawer class="dark accent-4" dark permanent app>
+    <v-list>
+      <v-list-item v-for="item in items" :key="item.title" link>
+        <v-list-item-icon>
+          <v-icon>{{ item.icon }}</v-icon>
+        </v-list-item-icon>
+
+        <v-list-item-content>
+          <v-list-item-title>{{ item.title }}</v-list-item-title>
+        </v-list-item-content>
+      </v-list-item>
+    </v-list>
+
+    <template v-slot:append>
+      <div class="pa-2">
+        <v-btn class="deep-orange" block> Logout </v-btn>
+      </div>
+    </template>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      items: [
+        { title: "Dashboard", icon: "mdi-view-dashboard" },
+        { title: "Photos", icon: "mdi-image" },
+        { title: "About", icon: "mdi-help-box" },
+      ],
+      right: null,
+    };
+  },
+};
+</script>
