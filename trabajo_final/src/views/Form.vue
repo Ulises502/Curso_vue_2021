8e36e1011cd6847c66b06afc1fcb074d036fb141
Ulises502 <43120036+Ulises502@users.noreply.github.com>
2021-06-09 13:56:56 -0300

    Crear primeras estructuras de form.vue

--- trabajo_final/src/views/Form.vue
+++ trabajo_final/src/views/Form.vue
@@ -1,12 +1,62 @@
 <template>
-  <div>
-    <v-btn block> Block Button </v-btn>
-  </div>
+  <v-form v-model="valid">
+    <v-container>
+      <v-row>
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="firstname"
+            :rules="nameRules"
+            :counter="10"
+            label="First name"
+            required
+          ></v-text-field>
+        </v-col>
+
+        <v-col cols="12" md="4">
+          <v-text-field
+            v-model="lastname"
+            :rules="nameRules"
+            :counter="10"
+            label="Last name"
+            required
+          ></v-text-field>
+        </v-col>
+      </v-row>
+      <br />
+      <v-btn class="mr-4" @click="submit"> submit </v-btn>
+      <v-btn @click="clear"> clear </v-btn>
+    </v-container>
+  </v-form>
 </template>
 
 <script>
 export default {
   name: "Form",
-  components: {},
+  data: () => ({
+    valid: false,
+    firstname: "",
+    lastname: "",
+    nameRules: [
+      (v) => !!v || "Name is required",
+      (v) => v.length <= 10 || "Name must be less than 10 characters",
+    ],
+    email: "",
+    emailRules: [
+      (v) => !!v || "E-mail is required",
+      (v) => /.+@.+/.test(v) || "E-mail must be valid",
+    ],
+  }),
+  methods: {
+    submit() {
+      this.$v.$touch();
+    },
+    clear() {
+      this.$v.$reset();
+      this.name = "";
+      this.email = "";
+      this.select = null;
+      this.checkbox = false;
+    },
+  },
 };
 </script>
